cb8acd98999aef7c7f6177a54db93f514e29ced6
alex <alex@slab.org>
2025-02-27 15:08:50 +0000

    not spinning yet

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import { jux } from '@strudel/core';
+import { NeoCyclist } from '@strudel/core/neocyclist.mjs';
+import { saw } from '@strudel/core';
+// import { getTrigger } from '@strudel/core/repl.mjs';
+import { setInterval, clearInterval } from 'worker-timers';
+import { getAudioContext } from '@strudel/webaudio';
+
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -13,14 +15,37 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-let ticks = 0;
+const record = document.getElementById('record');
+
+const schedulerOptions = {
+  onTrigger: x => console.log('bing'),
+  getTime: () => getAudioContext().currentTime,
+
+  // onToggle: (started) => {
+  // updateState({ started });
+  // onToggle?.(started);
+  // },
+  setInterval,
+  clearInterval,
+  // beforeStart,
+};
+window.cyclist = new NeoCyclist(schedulerOptions);
+cyclist.setPattern(saw.segment(16), true);
+
+record.onclick = () => {
+  if (!cyclist.started) {
+    cyclist.start();
+  }
+  console.log('aha', cyclist.getTime())
+}
+
 const timer = setInterval(function () {
-  const deg = ticks % 360;
-  const record = document.getElementById('record');
-  record.style.webkitTransform = 'rotate(' + deg + 'deg)';
-  record.style.mozTransform = 'rotate(' + deg + 'deg)';
-  record.style.msTransform = 'rotate(' + deg + 'deg)';
-  record.style.oTransform = 'rotate(' + deg + 'deg)';
-  record.style.transform = 'rotate(' + deg + 'deg)';
-  ticks++;
+  if (cyclist.started) {
+    const deg = (cyclist.cycle % 1) % 360;
+    record.style.webkitTransform = 'rotate(' + deg + 'deg)';
+    record.style.mozTransform = 'rotate(' + deg + 'deg)';
+    record.style.msTransform = 'rotate(' + deg + 'deg)';
+    record.style.oTransform = 'rotate(' + deg + 'deg)';
+    record.style.transform = 'rotate(' + deg + 'deg)';
+  }
 }, 50);
